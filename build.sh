6a35b6577d9f317e24d300d6878b1faa15b7623c
Nikolay Vasiliev <gozebone@gmail.com>
2025-01-25 12:24:17 +0300

    updated build for lsp

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-mkdir -p cmake-build-$1
-rm -rf cmake-build-$1/*
+mkdir -p build
+rm -rf build/*
 cmake "-DCMAKE_TOOLCHAIN_FILE=/Users/gozebone/vcpkg/scripts/buildsystems/vcpkg.cmake" --preset $1 \
-  -DENABLE_SLOW_TEST=ON -DTREAT_WARNINGS_AS_ERRORS=ON -S .
-cmake --build cmake-build-$1
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DENABLE_SLOW_TEST=ON -DTREAT_WARNINGS_AS_ERRORS=ON -S .
+cmake --build ./build
 
