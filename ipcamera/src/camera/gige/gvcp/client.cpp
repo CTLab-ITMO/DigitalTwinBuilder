ee286710ae49c093d75af052f06ce698aa4f7315
Nikolay Vasilev <gozebone@gmail.com>
2025-03-18 22:07:15 +0300

    sizes error in read and write

--- ipcamera/src/camera/gige/gvcp/client.cpp
+++ ipcamera/src/camera/gige/gvcp/client.cpp
@@ -30,16 +30,20 @@ uint16_t client::start_streaming(const std::string& rx_address, uint16_t rx_port
     if (!get_control()) {
         return 0;
     }
-    std::cout << "writing stream port and address" << '\n';
-    ack response_port_address_write = execute(cmd::writereg(req_id_++, {{registers::stream_channel_port_0, rx_port}, {registers::stream_channel_destination_address_0, boost::asio::ip::make_address_v4(rx_address).to_uint()}}));
-    if (response_port_address_write.get_header().status != status_codes::GEV_STATUS_SUCCESS) {
-        return 0;
-    }
     std::cout << "reading stream port" << '\n';
     ack response_stream_channel_source_port = execute(cmd::readreg(req_id_++, std::vector<uint32_t>{registers::stream_channel_source_port_0}));
     if (response_stream_channel_source_port.get_header().status != status_codes::GEV_STATUS_SUCCESS) {
         return 0;
     }
+    std::cout << "writing stream port and address" << '\n';
+    ack response_port_address_write = execute(cmd::writereg(req_id_++, {{registers::stream_channel_destination_address_0, boost::asio::ip::make_address_v4(rx_address).to_uint()}, {registers::stream_channel_port_0, rx_port} }));
+    if (response_port_address_write.get_header().status != status_codes::GEV_STATUS_SUCCESS) {
+        return 0;
+    }
+    do {
+        response_port_address_write = execute(cmd::readreg(req_id_++, std::vector<uint32_t>{registers::stream_channel_port_0}));
+        std::cout << "still" << std::endl;
+    } while (response_port_address_write.get_header().status == status_codes::GEV_STATUS_SUCCESS);
     return std::get<ack::readreg>(response_stream_channel_source_port.get_content()).register_data[0];
 }
 
