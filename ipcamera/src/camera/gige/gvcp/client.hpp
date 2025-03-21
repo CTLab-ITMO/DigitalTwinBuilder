45160aa715b6a56fa01a3e24b92f16bcb3a1b3af
Nikolay Vasiliev <gozebone@gmail.com>
2025-03-21 08:47:58 +0300

    gige client

--- ipcamera/src/camera/gige/gvcp/client.hpp
+++ ipcamera/src/camera/gige/gvcp/client.hpp
@@ -18,7 +18,7 @@ public:
     bool drop_control();
     uint16_t start_streaming(const std::string& rx_address, uint16_t rx_port);
     ack execute(const cmd::command& cmd);
-    const std::string& address() const;
+    const std::string& get_address() const;
 private:
     std::string address_;
     uint16_t req_id_ = 1;
