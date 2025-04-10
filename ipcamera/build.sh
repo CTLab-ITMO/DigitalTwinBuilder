0ec3dea184771b0930a050f10f69a95bcf5e64ac
Nikolay Vasiliev <gozebone@gmail.com>
2025-04-11 01:34:30 +0300

    build removed

--- ipcamera/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-mkdir -p build
-rm -rf build/*
-cmake "-DBOOST_ROOT=~/.local/opt/boost/" "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake" --preset $1 -S .
-cmake --build ./build
-
