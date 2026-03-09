016323503b711b2e8ec895b5dca61d4f139ca261
Nikolay Vasilev <gozebone@gmail.com>
2026-03-09 22:48:42 +0300

    clenup

--- tests/agents/ui/append-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 8 8);
-do
-    echo "testing tests/agents/ui/jsons/${i}.json"
-    ./.venv/bin/python tests/agents/ui/llm-responder.py tests/agents/ui/jsons/${i}.json
-done
-
